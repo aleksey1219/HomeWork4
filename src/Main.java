ca6b1b512ac68570b07f4c43212620c1cdffa2ac
DNS <glotov-aleks@mail.ru>
2023-04-20 18:17:40 +0500

    task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+    }
+
+    public static void task1() {
+        int age = 17;
+        if (age >= 18) {
+            System.out.println("Если возраст человека равен " + age + " то он совершеннолетний");
+        } else {
+            System.out.println("Если возраст человека равен " + age + " то он не достиг совершеннолетия ");
+        }
     }
 }
