1087045df2dbac88d80947a81221a2ef79b553fa
DNS <glotov-aleks@mail.ru>
2023-04-20 19:18:51 +0500

    task4

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
         task1();
         task2();
         task3();
+        task4();
     }
 
     public static void task1() {
@@ -32,6 +33,24 @@ public class Main {
             System.out.println("Если скорость " + speed + " км/ч , то придется заплатить штраф");
         } else {
             System.out.println("Если скорость " + speed + " км/ч ,то можно ехать спокойно ");
+        }
+    }
+
+    public static void task4() {
+        System.out.println("решение задачи №4");
+        int age = 18;
+        if (age < 2) {
+            System.out.println("Если человеку " + age + " то ему пора спать.");
+        } else if (age >= 2 && age < 6) {
+            System.out.println("Если человеку " + age + " то ему нужно ходить в детский сад.");
+        } else if (age >= 7 && age <= 18) {
+            System.out.println("Если человеку " + age + " то ему нужно ходить в школу");
+        } else if (age > 18 && age <= 24) {
+            System.out.println("Если человеку " + age + " то ему нужно ходить в университет");
+        } else if (age < 60 && age > 24) {
+            System.out.println("Если человеку " + age + " то ему нужно ходить на работу");
+        } else {
+            System.out.println("Если человеку " + age + " то ему можно отдохнуть");
         }
     }
 }
